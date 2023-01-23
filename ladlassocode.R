039ec54de70c1108d4961e8824574c44a2df74f2
lee1995hao <lee1995hao@gmail.com>
2023-01-23 15:13:55 +0800

    add the normal lcad regression method

--- ladlassocode.R
+++ ladlassocode.R
@@ -5,9 +5,10 @@ library(caret)
 library(quantreg)
 library(perryExamples)
 library(glmnet) 
+library(caret)
 
 
-##
+
 
 rho_a <- function (u, a = 2.68) ifelse(abs(u) < a, abs(u), a)
 skipped_median2 <- function (x, a = 2.68, prt = FALSE) {
@@ -27,9 +28,10 @@ skipped_median2 <- function (x, a = 2.68, prt = FALSE) {
 }
 
 
-lcad.r <- function(u, a){ifelse(abs(u) > a, u, 0)}
+lcad.r <- function(u, a){ifelse(abs(u)>a,u,0)}
 lcad.r <- Vectorize(lcad.r)
-iialcad <- function(y, x.data, a){
+##code for without lasso
+iialcad <- function(y,x.data,a){
   x <- x.data[,-1]
   beta <- lad(y ~ x,data = data.frame(y = y,x = x))$coefficients
   small.r = y - x.data%*%beta
@@ -43,30 +45,52 @@ iialcad <- function(y, x.data, a){
     y <- y[po]
     x.data <- x.data[po,]
     x <- x.data[,-1]
-    lambda <- seq(25, 10^(-25), length.out = 100)
-    splits <- splitControl(m = 40, R = 10)
-    new.beta.c <- ladlasso(x.data[,-1],y, lambda = lambda, splits = splits, seed = 2014)$finalModel$coefficients
-    new.beta <-sapply(new.beta.c, FUN = function(x){ifelse(abs(x) <= 0.0001, 0, x)})
+    new.beta <- lad(y ~ x)$coefficients
     judge.loop <-  sqrt(sum((beta - new.beta)^2))
     beta <- new.beta
     b = b+1
     if(judge.loop < 0.001|b == 1000)break
   }
-  
-  return(list(iialcad.beta = beta,nomal_data = x.data))
+  return(list(iialcad.beta = beta,a = a,b = b))
 }
 
 
 
+##code with lasso
+iialcad_lasso <- function(y, x.data, a){
+  x <- x.data[,-1]
+  beta <- lad(y ~ x,data = data.frame(y = y,x = x))$coefficients
+  small.r = y - x.data%*%beta
+  sigma = mad(small.r, center = skipped_median2(small.r)$m)
+  b = 0
+  small.r.hat <- c()
+  repeat{
+    z = (y - x.data%*%beta)/sigma
+    small.r.hat <- lcad.r(u = z, a = a)
+    po <- which(small.r.hat == 0)
+    y <- y[po]
+    x.data <- x.data[po,]
+    x <- x.data[,-1]
+    result <- cv.hqreg(x,y,type.measure = "mae")
+    best.L.idx <- which(result$lambda == result$lambda.min)
+    new.beta <- result$fit$beta[,best.L.idx]
+    judge.loop <-  sqrt(sum((beta - new.beta)^2))
+    beta <- new.beta
+    b = b + 1
+    if(judge.loop < 0.001|b == 1000)break
+  }
+  
+  return(list(iialcad.beta = beta,a = a,b = b))
+}
 
 
-
-
-
-
-
-
-
-
+LCAD <- function(y, x.data, a, method = "lasso"){
+  if(method == "lasso"){
+    real_result <- iialcad_lasso(y, x.data, a)
+  }else{
+    real_result <- iialcad(y, x.data, a)
+  }
+  return(real_result)
+}
 
 
